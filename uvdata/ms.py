99afbb2deb65e29f593436489b52a45c9fd158d4
Aaron Ewall-Wice <aaronew@Aarons-MBP.home>
2016-12-15 09:55:09 +0800

    nearing completion of first draft of reading measurement sets

--- uvdata/ms.py
+++ uvdata/ms.py
@@ -5,8 +5,10 @@ import numpy as np
 import warnings
 from uvdata import UVData
 import parameter as uvp
-import casacore
-import casacore.tables
+import casacore.tables as tables
+import telescopes
+
+polDict={1:1,2:2,3:3,4:4,5:-1,6:-2,7:-3,8:-4,9:-4,10:-5,11:-6,12:-8}
 
 
 class MS(UVData):
@@ -39,6 +41,7 @@ class MS(UVData):
         self.freq_array.value=freqs
         self.Nfreqs.value=int(freqs.shape[1])
         self.channel_width.value=tb_spws.getcol('CHAN_WIDTH')[0,0]
+        self.Nspws.value=int(freqs.shape[0])
         self.spw_array.value=n.arange(self.Nspws.value)
         tb_spws.close()
         
@@ -60,4 +63,46 @@ class MS(UVData):
         self.ant_1_array.value=tb.getcol('ANTENNA1').astype(int32)
         self.ant_2_array.value=tb.getcol('ANTENNA2').astype(int32)
         self.basline_array.value=self.antnums_to_baseline(self.ant_1_array.value,self.ant2_array.value)
-        #get polarization time
+        #Get times. MS that I'm used to are modified Julian dates in seconds (thanks to Danny Jacobs for figuring out the proper conversion)
+        self.time_array.value=time.Time(tb.getcol('TIME')/(3600.*24.),format='mjd').jd
+        #Polarization array
+        tbPol=tables.table(filename+'.ms/POLARIZATION')
+        self._polarization_array.value=polDict[tbPOl.getcol('CORR_TYPE')].astype(int)
+        tbPol.close()
+        #Integration time
+        #use first interval and assume rest are constant (though measurement set has all integration times for each Nblt )
+        self._integration_time.value=tb.getcol('INTERVAL')[0]
+
+
+        #open table with antenna location information
+        tbAnt=tables.table(filename+'.ms/ANTENNA')
+        #antenna names
+        self._antenna_names.value=tbAnt.getcol('NAME')
+        #Source Field
+        self._antenna_numbers.value=np.unique(tbAnt.getcol('ANTENNA1')).astype(int)
+        #Telescope Name
+
+        #Instrument
+        self._instrument.value=tbAnt.getcol('STATION')[0]
+        self._telescope_name.value=tbAnt.getcol('STATION')[0]
+        #Use Telescopes.py dictionary to set array position
+        try:
+            thisTelescope=telescopes.get_telescope(self.instrument.value)
+            self.telescope_location_lat_lon_alt_degrees.value=(np.degrees(thisTelescope['latitude']),np.degrees(thisTelescope['longitutde']),thisTelescope['altitude'])
+        except:
+            #If Telescope is unknown, use median ITRF Positions of antennas
+            self.telescope_location.value=np.array([np.median(tbAnt.getcol('POSITION')[:,mm]) for mm in range(3)])
+        tbAnt.close()
+        tbField=tables.table(filename+'.ms/FIELD')
+        if(tbField.getcol('PHASE_DIR').shape[1]>1):
+            self._phase_type.value='drift'
+        elif(tbField.getcol('PHASE_DIR').shape[1]==1):
+            self._phase_type.value='phased'
+        #else:
+        #    self._phase_type.value='unknown'
+        #set LST array from times and itrf
+        self.set_lsts_from_time_array()
+            
+            
+        
+        
