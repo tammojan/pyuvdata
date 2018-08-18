38409759f921a9b3e1317aa1ccf847c1b976ad7a
EXTERNAL-Ewall-Wice <aaron.m.ewall-wice@jpl.nasa.gov>
2018-08-18 14:39:39 -0700

    testing write_transfer

--- pyuvdata/mmode.py
+++ pyuvdata/mmode.py
@@ -11,127 +11,79 @@ requires the driftscan library to be installed
 from caput import config
 from caput import time as ctime
 from uvdata import UVData
-import uvbeam as UVBeam
+from uvbeam import UVBeam
 from drift.core.telescope import SimplePolarisedTelescope
 from pyuvsim import AnalyticBeam
 from drift.core.beamtransfer import BeamTransfer
+C = 299792458. #speed of light
 
-class UVDataDrift():
-    """
-    Class Defining a uvdata+uvbeam object for export to a DriftScanTelescope
-    object used in m-mode analyses.
-    https://github.com/radiocosmology/driftscan/blob/master/drift/core/telescope.py
-    """
+#create abstract telescope class
+class UVPolarisedTelescope(SimplePolarisedTelescope):
+    #TODO: Implement multiple beams for multiple tiles.
+    def __init__(self, uvdata, uvbeams, uv_res = 0.5):
+        self.uvdata = uvdata
+        self.uvbeams = uvbeams
+        self.uv_res = uv_res
+        self.min_lambda = C / uvdata.freq_array.max()
 
+    def beamx(self, feed, freq):
+        """Beam for the X polarisation feed.
 
-    def __init__(self):
-        """Create a new UVData2Mmode"""
-        self.uvbeam = None
-        self.uvdata = None
-        self.beam_transfer = None
+        Parameters
+        ----------
+        feed : integer
+            Index for the feed.
+        freq : integer
+            Index for the frequency.
 
-    def set_beams(self,beams):
+        Returns
+        -------
+        beam : np.ndarray
+            Healpix maps (of size [self._nside, 2]) of the field pattern in the
+            theta and phi directions.
         """
-        Set uvbeam object with a single beam or list of beams (for each antenna beam)
-        Args:
-            beams, UVBeam or AnalyticBeam object (for homogenous arrays) or list of UVBeam objects
-            giving the beam at each antenna. Number of beam objects should Equal
-            the number of antennas in the data set.
+        return self.uvbeams[feed].interp(self._angpos[:,1],self._angpos[:,0],freq)[0][:,0,0,0,:].squeeze().T
+
+    def beamy(self, feed, freq):
+        """Beam for the Y polarisation feed.
+
+        Parameters
+        ----------
+        feed : integer
+            Index for the feed.
+        freq : integer
+            Index for the frequency.
+
+        Returns
+        -------
+        beam : np.ndarray
+            Healpix maps (of size [self._nside, 2]) of the field pattern in the
+            theta and phi directions.
         """
-        if isinstance(beams,UVBeam) or isinstance(beams, AnalyticBeam):
-            self.uvbeam = [beams for beams in len(self.uvdata.Nants_data)]
-        else:
-            if len(beams) != self.uvdata.Nants_data:
-                raise ValueError('Number of beams in beam list must equal number of antennas in data set.')
-            else:
-                self.uvbeam = beams
-    def set_uv(self,uvdatasets):
-        """
-        Set uvdata object (or list of uvdata objects that will be cominbed).
-        Args:
-            uvdatasets, a UVData object or list of UVData objects
-        """
-        #TODO: Enforce identical antenna data and frequencies.
-        if isinstance(uvdatasets,UVData):
-            self.uvdata = uvdatasets
-        elif isinstance(uvdatasets, list)
-            self.uvdata = uvdatasets[0]
-            for uvd in uvdatasets[1:]:
-                self.uvdata.add(uvd)
-        else:
-            raise ValueError("Must provide a UVData object or list of UVData objects")
+        return self.uvbeams[feed].interp(self._angpos[:,1],self._angpos[:,0],freq)[0][:,0,1,0,:].squeeze().T
+       # Set the feed array of feed positions (in metres EW, NS)
+    @property
+    def _single_feedpositions(self):
+        #Do DriftScanTelescopes support 3d antenna positions?
+        pos = self.uvdata.get_ENU_antpos(pick_data_ants=True)[0][:,:-1]
+        return pos
 
-    def from_transit_telescope_and_timestream():
-        '''
-        create uvdata object from transit telescope object and time-stream data
-        '''
-        return None
+    # Give the widths in the U and V directions in metres (used for
+    # calculating the maximum l and m)
+    @property
+    def u_width(self):
+        return self.uv_res * self.min_lambda
 
-    def write_timestream(self):
-        '''
-        export uvdata object to an mmode time-stream
-        '''
-        return None
-
-    def write_transfer_matrices(self,directory):
-        '''
-        Write out transfer matrices
-        '''
-        self.beam_transfer = BeamTransfer()
+    @property
+    def v_width(self):
+        return self.uv_res * self.min_lambda
 
 
 
-    def to_transit_telescope(self, directory):
-        """Telescope Export beam transfer matrix"""
-        #check that self.uvdata and self.uvbeam have been properly initialized
-        self.uvdata.check()
-        self.uvbeam.check()
-        lat,lon,alt=self.uvdata.telescope_location_lat_lon_alt_degrees
+class BeamTransferer():
+    def __init__(self, uvdata, uvbeams, directory = '.', uv_res = 0.5):
+        my_telescope = UVPolarisedTelescope(uvdata = uvdata, uvbeams = uvbeams, uv_res = uv_res)
+        self.beam_transfer = BeamTransfer(directory=directory, telescope = my_telescope)
 
-        #create abstract telescope class
-        class MyPolarisedTelescope(SimplePolarisedTelescope):
-            #TODO: Implement multiple beams for multiple tiles.
-            def beamx(feed, freq):
-                """Beam for the X polarisation feed.
-
-                Parameters
-                ----------
-                feed : integer
-                    Index for the feed.
-                freq : integer
-                    Index for the frequency.
-
-                Returns
-                -------
-                beam : np.ndarray
-                    Healpix maps (of size [self._nside, 2]) of the field pattern in the
-                    theta and phi directions.
-                """
-                return self.uvbeam[feed].interp(self._angpos[:,1],self._angpos[:,0],freq)[:,0,0,0,:].squeeze().T
-            def beamy(feed, freq):
-                """Beam for the Y polarisation feed.
-
-                Parameters
-                ----------
-                feed : integer
-                    Index for the feed.
-                freq : integer
-                    Index for the frequency.
-
-                Returns
-                -------
-                beam : np.ndarray
-                    Healpix maps (of size [self._nside, 2]) of the field pattern in the
-                    theta and phi directions.
-                """
-                return self.uvbeam[feed].interp(self._angpos[:,1],self._angpos[:,0],freq)[:,0,1,0,:].squeeze().T
-               # Set the feed array of feed positions (in metres EW, NS)
-        @property
-        def _single_feedpositions(self):
-            #Do DriftScanTelescopes support 3d antenna positions?
-            pos = self.uvdata.get_ENU_antpos(pick_data_ants=True)[:,:-1]
-            return pos
-
-        self.beam_transfer = BeamTransfer()
-        self.beam_transfer.telescope = MyPolarisedTelescope()
-        self.beam_transfer.directory = directory
+    def write_transfer(self):
+        self.beam_transfer.generate()
--- pyuvdata/tests/test_mmode.py
+++ pyuvdata/tests/test_mmode.py
@@ -2,7 +2,6 @@
 Tests for mmode transfer matrix translation
 """
 
-testdir = os.path.join(DATA_PATH, 'mmode_test_data/')
 import nose.tools as nt
 import os
 from pyuvdata import UVData
@@ -10,8 +9,10 @@ import pyuvdata.utils as uvutils
 import pyuvdata.tests as uvtest
 from pyuvdata.data import DATA_PATH
 import numpy as np
-from pyuvdata.mmode import UVDataDrift
+from pyuvdata.mmode import BeamTransferer
 from pyuvsim import AnalyticBeam
+testdir = os.path.join(DATA_PATH, 'test/')
+
 
 def test_write_transfer_matrices():
     '''
@@ -22,6 +23,6 @@ def test_write_transfer_matrices():
     uv_uniform = UVData()
     uv_uniform.read_uvfits(os.path.join(testdir,'two_antennas_equator_uniform.uvfits'))
     uv_beams_uniform = [ AnalyticBeam('uniform'), AnalyticBeam('uniform') ]
-    uv_drift_uniform = UVDataDrift()
-    uv_drift_uniform.set_beams(uv_beams_uniform)
-    uv_drift_uniform.set_uv(uv_uniform)
+    outdir = os.path.join(testdir, 'mmode_out_uniform')
+    uv_drift_uniform = BeamTransferer(uvdata = uv_uniform, uvbeams = uv_beams_uniform, directory = outdir)
+    uv_drift_uniform.write_transfer()
